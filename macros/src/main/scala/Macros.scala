359449aba3ba6223135ec23b12e792e11d68e984
Eugene Burmako <xeno.by@gmail.com>
2014-05-10 18:05:02 +0200

    Update Macros.scala

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -4,7 +4,7 @@ import scala.reflect.macros.Context
 object Macros {
   def impl(c: Context) = {
     import c.universe._
-    q"""println("Hello World")"""
+    c.Expr[Unit](q"""println("Hello World")""")
   }
 
   def hello: Unit = macro impl
