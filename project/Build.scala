f38d7796b59044814d1b3322a6501780ebee5c13
Eugene Burmako <xeno.by@gmail.com>
2014-08-01 15:54:51 +0200

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,8 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.scalamacros",
     version := "1.0.0",
-    scalaVersion := "2.11.1",
-    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0", "2.11.1"),
+    scalaVersion := "2.11.2",
+    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0", "2.11.1", "2.11.2"),
     resolvers += Resolver.sonatypeRepo("snapshots"),
     resolvers += Resolver.sonatypeRepo("releases"),
     scalacOptions ++= Seq()
