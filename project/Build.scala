1dc5be3fb6015d5f22390ea24ef0dc8f09d6963e
Eugene Burmako <xeno.by@gmail.com>
2014-10-07 16:20:52 +0200

    support for quasiquotes for 2.10.x

--- project/Build.scala
+++ project/Build.scala
@@ -27,7 +27,20 @@ object MyBuild extends Build {
     "macros",
     file("macros"),
     settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _))
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
+      libraryDependencies := {
+        CrossVersion.partialVersion(scalaVersion.value) match {
+          // if Scala 2.11+ is used, quasiquotes are available in the standard distribution
+          case Some((2, scalaMajor)) if scalaMajor >= 11 =>
+            libraryDependencies.value
+          // in Scala 2.10, quasiquotes are provided by macro paradise
+          case Some((2, 10)) =>
+            libraryDependencies.value ++ Seq(
+              compilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full),
+              "org.scalamacros" %% "quasiquotes" % "2.0.0" cross CrossVersion.binary)
+        }
+      }
+    )
   )
 
   lazy val core: Project = Project(
