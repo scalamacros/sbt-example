7de1daf4d565059f4461947688308450a81d7140
Eugene Burmako <xeno.by@gmail.com>
2015-03-03 11:23:04 +0100

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object BuildSettings {
     organization := "org.scalamacros",
     version := "1.0.0",
     scalaVersion := "2.11.6",
-    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0", "2.11.1", "2.11.2", "2.11.3", "2.11.4", "2.11.5", "2.11.6"),
+    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.10.5", "2.11.0", "2.11.1", "2.11.2", "2.11.3", "2.11.4", "2.11.5", "2.11.6"),
     resolvers += Resolver.sonatypeRepo("snapshots"),
     resolvers += Resolver.sonatypeRepo("releases"),
     scalacOptions ++= Seq()
